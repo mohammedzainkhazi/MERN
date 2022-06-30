5ff0866a9b4033c311f5611f663b5604c1415e25
mohammedzainkhazi <zainhamza@outlook.com>
2022-06-30 21:17:00 +0530

    Added All Files

---
+++ client/build/precache-manifest.0a9de53b1bac400f751789328081ea7e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5b449f20fa7880eb81eecdb07ecb38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "666c9f44fe739f9d4360",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "0558e9cdd37d055745eb",
+    "url": "/static/css/main.69f0b9f1.chunk.css"
+  },
+  {
+    "revision": "666c9f44fe739f9d4360",
+    "url": "/static/js/2.9a9d2830.chunk.js"
+  },
+  {
+    "revision": "d1e5e671d302c246c8665e804545ac71",
+    "url": "/static/js/2.9a9d2830.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0558e9cdd37d055745eb",
+    "url": "/static/js/main.19525f32.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "abb6438184ca99809735063a964f4840",
+    "url": "/static/media/Diary.abb64381.jpg"
+  },
+  {
+    "revision": "b4f387b7e793122fe934e36e6e1432ba",
+    "url": "/static/media/logo5.b4f387b7.jpg"
+  }
+]);
